4d75d3598ac160f7a2816b2732a3a18dc9c5a926
Alexander Georgiev <alexfromsofia@gmail.com>
2020-04-12 14:49:34 +0300

    Add files using multer and throw custom middleware error on file validation

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const router = new express.Router();
 
 const authMiddleware = require("../middleware/authentication");
@@ -103,5 +104,30 @@ router.delete("/users/me", authMiddleware, async (req, res) => {
         res.status(500).send(error);
     }
 });
+
+const upload = multer({
+    dest: "avatars",
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error("Please upload a jpg, jpeg or png image."));
+        }
+
+        return cb(undefined, true);
+    },
+});
+
+router.post(
+    "/users/me/avatar",
+    upload.single("avatar"),
+    (req, res) => {
+        res.send();
+    },
+    (error, req, res, next) => {
+        res.status(400).send({ error: error.message });
+    }
+);
 
 module.exports = router;
