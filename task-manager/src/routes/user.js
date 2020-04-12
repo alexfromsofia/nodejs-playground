b5c7e9aa79fd37c070c5b9ea44c7da0f98ef7f2b
Alexander Georgiev <alexfromsofia@gmail.com>
2020-04-12 15:51:31 +0300

    Add sharp for image processing before saving avatar

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 const router = new express.Router();
 
 const authMiddleware = require("../middleware/authentication");
@@ -123,7 +124,14 @@ router.post(
     authMiddleware,
     upload.single("avatar"),
     async (req, res) => {
-        req.user.avatar = req.file.buffer;
+        const buffer = await sharp(req.file.buffer)
+            .resize({
+                width: 250,
+                height: 250,
+            })
+            .png()
+            .toBuffer();
+        req.user.avatar = buffer;
         await req.user.save();
 
         res.send();
@@ -148,7 +156,7 @@ router.get("/users/:id/avatar", async (req, res) => {
             throw new Error();
         }
 
-        res.set("Content-Type", "image/jpg");
+        res.set("Content-Type", "image/png");
         res.send(user.avatar);
     } catch (error) {
         res.status(404).send();
